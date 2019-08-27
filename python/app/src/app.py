5de0c2b14a160f170bb037c23583f7230672abb1
suprematis <as@andresalgado.com>
2019-08-27 08:17:51 -0700

    Returned code to origina state to branch new approach

--- python/app/src/app.py
+++ python/app/src/app.py
@@ -3,46 +3,54 @@ from flask import Flask
 from flask import request
 import subprocess
 
-db=mdb.connect('db','flaskuser','flask123','flasktest')
+db=mdb.connect('mariadb','flaskuser','flask123','flasktest')
 app = Flask('flask-mysql')
 #ip_whitelist = ['127.0.0.1']
 query_success = "SELECT COUNT(*) FROM flasktest.tasks WHERE task_status='Success'"
-query_pending = "SELECT COUNT(*) FROM flasktest.tasks WHERE task_status='Pending'"
-query_failed = "SELECT COUNT(*) FROM flasktest.tasks WHERE task_status='Failed'"
+#query_pending = "SELECT COUNT(*) FROM flasktest.tasks WHERE task_status='Pending'"
+#query_failed = "SELECT COUNT(*) FROM flasktest.tasks WHERE task_status='Failed'"
 cur = db.cursor()
-def sql_queries(ref):
-     def success():
-         cur.execute(query_success)
-         return query_success
-     def pending():
-         return query_pending
-     def failed():
-         return query_failed
-     if ref == 1:
-         return success
-     else:
-         return pending
-    	
+
+def valid_ip():
+#    client = request.remote_addr
+#    if client in ip_whitelist:
+     return True
+#    else:
+#        return False
+
 
 @app.route('/status/')
-def get_status( success, pending ):
-        return '%s , %s' % ( success, pending );
+def get_status():
+    if valid_ip():
+        command_success = cur.execute("SELECT COUNT(*) FROM flasktest.tasks WHERE task_status='Success'") 
+#        command_success = "docker exec mariadb mysql -uflaskuser -pflask123 -e '{0}'".format(
+#            query_success)
+#        command_pending = "docker exec mariadb mysql -uflaskuser -pflask123 -e '{0}'".format(
+#            query_pending)
+#        command_failed = "docker exec mariadb mysql -uflaskuser -pflask123 -e '{0}'".format(
+#            query_failed)
+
+        try:
+            command_success
+#            result_success = subprocess.check_output(
+#                [command_success], shell=True)
+#            result_pending = subprocess.check_output(
+#                [command_pending], shell=True)
+#            result_failed = subprocess.check_output(
+#                [command_failed], shell=True)
+        except subprocess.CalledProcessError as e:
+            return "An error occurred while trying to fetch task status updates."
 
 
- 
-pending = cur.execute(query_pending)
-get_status( success, pending ) 
-
-print ( success, pending )
-#return 'Success %s, Pending %s' % (command_success, command_pending)
+        return 'Success %s' % (command_success)
 #        return 'Success %s, Pending %s, Failed %s' % (result_success, result_pending, result_failed)
-#    else:
-#        return """<title>404 Not Found</title>
-#               <h1>Not Found</h1>
-#               <p>The requested URL was not found on the server.
-#               If you entered the URL manually please check your
-#               spelling and try again.</p>""", 404
-#
+    else:
+        return """<title>404 Not Found</title>
+               <h1>Not Found</h1>
+               <p>The requested URL was not found on the server.
+               If you entered the URL manually please check your
+               spelling and try again.</p>""", 404
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
